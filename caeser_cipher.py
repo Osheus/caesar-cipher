b922602bdad51d8deb8e6798bf59d6539472a106
Joshua <115429211+Osheus@users.noreply.github.com>
2025-12-28 19:15:09 +0000

    Quality of life changes
    Made it so the program continues to run and doesn't switch off whenever you get to the end of a branch using while loops.
    Cleaned the output up, so it looked more consistent.
    Added a few comments here and there.
    Made it so it lets you know what the current status of the key and retain_spaces is.

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,11 +1,12 @@
 import random
 
-# ------------------------  FUNCTIONS ------------------------ 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+# ------------------------  FUNCTIONS ------------------------ 
+
+# Responsible for incrementing the letter by one value.
 def rotate_dial(character):
-    
     new_character = ''
 
     if character == 'z':
@@ -21,6 +22,7 @@ def rotate_dial(character):
     
     return new_character
 
+# Checks if a value is valid. Either part of the alphabet or a space.
 def on_dial_check(character):
     on_dial = False
     if character in alphabet:
@@ -29,8 +31,10 @@ def on_dial_check(character):
         on_dial = True
     return on_dial
 
+# Takes user input but only progesses to the next stage if every character is valid.
 def cipher_input():
     verified_input = False
+
     while not verified_input:
         user_input = input("Enter message here:  ").lower()
         test = []
@@ -41,21 +45,29 @@ def cipher_input():
         
         if all(test):
             verified_input = True
+
         else:
-            print("Illegal character present.\nAre you just using characters from the alphabet?\n\n")
+            print("\n-- Invalid character present. Please review the disclaimer.\n")
+    
     return user_input
 
-
-
+# Disclaimer message to appear before cipher or plaintext input. To help the user.
 def disclaimer():
     message = '''
     Disclaimer:
-    - Special characters and numbers are not prohibted.
+    - Special characters and numbers are prohibted.
     - All text will be made lower case.
     '''
     print(message)
 
+# Error message adapable to a differing number of options.
+def incorrect_option(no_of_options):
+    options = list(range(1, no_of_options + 1))
+    message = f"-- You didn't pick a valid option. Please pick from the following option: {options} --\n"
+    print(message)
+
 # ------------ MAIN MENU ------------ 
+# Main title art.
 def main_display():
     title = '''
    ___                               ___ _       _               
@@ -68,6 +80,7 @@ def main_display():
     
     print(title)
 
+# Available options from the main menu.
 def main_display_choices():
     options = '''
     Would you like to:
@@ -77,27 +90,36 @@ def main_display_choices():
     '''
     print(options)
 
-def main_display_branches():
-    main_display_choice = input("Enter selection here:  ")
-
-    if main_display_choice == '1':
-        encr_menu()
-    
-    elif main_display_choice == '2':
-        print('Here is where we would run the decryption function.')
-    
-    elif main_display_choice == '3':
-        print('This will be an else function, that exits the program.')
-    
-    else:
-        print('For now, this is an output to state you have entered an incorrect choice.')
-
+# The meat and potatoes of the main menu. 
 def main_menu():
-    main_display()
-    main_display_choices()
-    main_display_branches()
+    main_loop_active = True
+    error_message = False
+
+    while main_loop_active:
+        main_display()
+        main_display_choices()
+        
+        if error_message:
+            incorrect_option(3)
+            error_message = False
+        
+        main_display_choice = input("Enter selection here:  ")
+
+        if main_display_choice == '1':
+            encr_menu()
+    
+        elif main_display_choice == '2':
+            print('Here is where we would run the decryption function.')
+    
+        elif main_display_choice == '3':
+            main_loop_active = False
+            print("\n------ Closing program. Farewell! ------\n")
+    
+        else:
+            error_message = True
 
 # ------------ ENCRYPTION ------------ 
+# Encryption menu art.
 def encr_title_display():
     title = '''
    __                            _   _             
@@ -109,32 +131,51 @@ def encr_title_display():
     '''
 
     print(title)
-    
+
+def encr_current_selection(key_value='', spaces_verdict=''):
+    print(f"Key: {key_value}\t\tKeep Spaces: {spaces_verdict}")
+
+# Available options for the encryption menu
 def encr_key_choices():
     options = '''
     Would you like to:
     1. Provide a key
     2. Have a key randomly generated
-    3. Leave
     '''
     print(options)
 
+def encr_key_rules():
+    rules = '''
+    Guidelines for the key:
+    - The key must be an integer.\n
+    '''
+    print(rules)
+
+# Menu that appears to query whether the user wants to provide a key, or have one be randomly provided.
 def encr_key_branch():
-    encr_key_choice = input("Enter selection here:  ")
-    if encr_key_choice == '1':
-        return False
+    error_message = False
+    key_menu_loop = True
 
-    elif encr_key_choice == '2':
-        return True
+    while key_menu_loop:
+        if error_message:
+            encr_title_display()
+            encr_current_selection()
+            encr_key_choices()
+            incorrect_option(2)
+            error_message = False
 
-    elif encr_key_choice == '3':
-        print("Here we would go back to the main menu.")
-        exit()
+        encr_key_choice = input("Enter selection here:  ")
 
-    else:
-        print('For now, this is an output to state you have entered an incorrect choice.')
-        exit()
+        if encr_key_choice == '1':
+            return False
 
+        elif encr_key_choice == '2':
+            return True
+
+        else:
+            error_message = True
+
+# Provides the key to the cipher. Whether that be user generated or random.
 def acquire_key():
     generate_key = encr_key_branch()
     if generate_key:
@@ -142,14 +183,21 @@ def acquire_key():
     
     elif not generate_key:
         non_valid_key = True
+        encr_title_display()
+        encr_current_selection()
+        encr_key_rules()
+
         while non_valid_key:
             try:
-                key = int(input("Please enter your key here:    "))
+                key = int(input("Enter your key here:    "))
 
             # Defensive programming added to prevent a non integer from being added.
             # Will repeat until one is entered.    
             except ValueError:
-                print("\nProvided key must be an integer.")
+                encr_title_display()
+                encr_current_selection()
+                encr_key_rules()
+                print("-- Provided key must be an integer. --\n")
             else:
                 non_valid_key = False
     
@@ -160,25 +208,30 @@ def encr_space_choices():
     Would you like to:
     1. Retain spaces
     2. Remove spaces
-    3. Leave
     '''
     print(options)
 
 def encr_space_branch():
-    encr_space_choice = input("Enter selection here:  ")
-    if encr_space_choice == '1':
-        return True
+    error_message = False
+    space_menu_loop = True
 
-    elif encr_space_choice == '2':
-        return False
+    while space_menu_loop:
+        if error_message:
+            encr_title_display()
+            encr_current_selection(key)
+            encr_space_choices()
+            incorrect_option(2)
+            error_message = False
 
-    elif encr_space_choice == '3':
-        print("Here we would go back to the main menu.")
-        exit()
+        encr_space_choice = input("Enter selection here:  ")
+        if encr_space_choice == '1':
+            return True
 
-    else:
-        print('For now, this is an output to state you have entered an incorrect choice.')
-        exit()
+        elif encr_space_choice == '2':
+            return False
+
+        else:
+            error_message = True
 
 def encryption():
     cleartext = cipher_input()
@@ -195,23 +248,30 @@ def encryption():
             midtext = midtext + new_character
             ciphertext = midtext
     
-    print("\nEncryption Summary:")
-    print(f"The cleartext: {cleartext}")
-    print(f"The ciphertext: {ciphertext}")
-    print(f"Key: {key}")
+    encr_title_display()
+    print("\n-- Encryption Summary --")
+    print(f"Cleartext:\t{cleartext}")
+    print(f"Ciphertext:\t{ciphertext}")
+    print(f"Key:\t\t{key}")
+    print(f"Keep spaces:\t{retain_spaces}")
+    input("\nPress enter to proceed.")
 
-    
 def encr_menu():
     encr_title_display()
+    encr_current_selection()
 
     encr_key_choices()
     global key 
     key = acquire_key()
 
+    encr_title_display()
+    encr_current_selection(key)
     encr_space_choices()
     global retain_spaces 
     retain_spaces = encr_space_branch()
 
+    encr_title_display()
+    encr_current_selection(key,retain_spaces)
     disclaimer()
     encryption()
 
